d805468da6d60aa2e1a6efccd10900ddfb632de6
muqadar-ali <jmuqadarali@gmail.com>
2022-02-08 12:11:10 +0500

    Finished Gatsby Tutorial Part 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
           name: `content`,
           path: `${__dirname}/content`,
         }
-      }
+      },
+      "gatsby-plugin-mdx"
     ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,24 @@ import * as React from "react"
 import Layout  from "../components/layout"
 import { StaticImage } from 'gatsby-plugin-image'
 import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
+
 
 const IndexPage = ({data}) =>{
   return (
     <Layout pageTitle="Home">
       <p>This is my home page using gatsby tutorial </p>
-      <ul>
       {
-        data.allFile.nodes.map(node => (
-          <li key={node.id}>
-            {node.name}
-          </li>
+        data.allMdx.nodes.map((node) => (
+          <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <MDXRenderer>
+              {node.body}
+            </MDXRenderer>
+          </article>
         ))
       }
-      </ul>
       <StaticImage 
         src="../images/profile.jpg"
         alt="My pic"
@@ -31,10 +35,14 @@ const IndexPage = ({data}) =>{
 
 export const query = graphql`
   query {
-    allFile(filter: {sourceInstanceName: {eq: "content"}}) {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
         id
+        body
       }
     }
   }
